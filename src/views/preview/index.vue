e7f1df2d5d1de43a64dfc5cebf2fc8fb23ee76af
_zhiqiu <guojikun486546@163.com>
2025-10-27 14:56:53 +0800

    feat: 禁用webview 默认的右键菜单；增加 Esc 关闭窗口功能

--- src/views/preview/index.vue
+++ src/views/preview/index.vue
@@ -1,4 +1,21 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { getCurrentWebviewWindow } from '@tauri-apps/api/webviewWindow'
+import { useEventListener } from '@vueuse/core'
+import { onMounted } from 'vue'
+
+const appWindow = getCurrentWebviewWindow()
+
+onMounted(() => {
+    useEventListener('contextmenu', (e: MouseEvent) => {
+        e.preventDefault()
+    })
+    useEventListener('keydown', (e: KeyboardEvent) => {
+        if (e.key === 'Escape') {
+            appWindow.close()
+        }
+    })
+})
+</script>
 
 <template>
     <router-view v-slot="{ Component }">
